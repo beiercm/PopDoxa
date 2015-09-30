d77117148a657ef02bc4593f82ab033e17528598
DarthEnchilada <beiercm@gmail.com>
2015-09-29 20:46:10 -0400

    Changed build_tables.py

---
+++ web_scripts/get_user_opinions.php
@@ -0,0 +1,28 @@
+<?php
+	include 'get_connection.php';	
+
+	try {
+		if(PHP_SAPI === 'cli')
+		{
+			$user_id = $argv[1];
+		}
+		else
+		{
+			$user_id = $_GET['user_id'];
+		}
+
+		get_opinions($conn, $user_id);
+	}
+	catch (PDOException $e)
+	{
+		echo "Connection failed: " . $e->getMessage();
+		return;
+	}
+
+	$conn = null;
+
+	function get_opinions($conn, $user_id)
+	{
+		
+	}
+?>
