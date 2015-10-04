0a52b453846cc825cb9a756aac1a8ea9ec62cfc2
DarthEnchilada <beiercm@gmail.com>
2015-10-04 19:40:43 -0400

    get_posts

--- web_scripts/get_posts.php
+++ web_scripts/get_posts.php
@@ -37,25 +37,27 @@
 	function get_posts($conn, $state_id, $county_id, $city_id)
 	{
 
-		if($county != -1)
+		if($county_id != -1)
 		{
-		$query= $conn->prepare("SELECT posts.id from posts join counties on posts.counties = counties.id where  posts.county = :county_id AND posts.city = -1;");
-		$query->bindparam(':county_id', $county_id);
+			$query= $conn->prepare("SELECT posts.id from posts join counties on posts.counties = counties.id where  posts.county = :county_id AND posts.city = -1;");
+			$query->bindparam(':county_id', $county_id);
 		}
 
-		if($city != -1)
+		if($city_id != -1)
 		{
 			$query = $conn->prepare("SELECT posts.id from posts join cities on posts.city = cities.id where  posts.city =  :city_id;");
 			$query->bindparam(':city_id', $city_id);
 		}
 
-		if($county == -1 && $city == -1)
+		if($county_id == -1 && $city_id == -1)
 		{
 			$query = $conn->prepare("SELECT posts.id from posts join states on posts.state = states.id where states.id = :state_id AND posts.county = -1 AND posts.city = -1;");
 			$query->bindparam(':state_id', $state_id);
 		}
 
-		$result = $conn->query($query)->fetchAll(PDO::FETCH_ASSOC);
+		$query->execute();
+
+		$result = $query->fetchAll(PDO::FETCH_ASSOC);
 
 		$return = array();
 
