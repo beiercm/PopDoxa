5eeaf6388ae6cd805623f0fca3c37b30c85a2407
Christopher <beiercm@gmail.com>
2015-10-07 17:40:30 -0400

    Changed get_recent_posts

---
+++ web_scripts/get_recent_posts.php
@@ -0,0 +1,64 @@
+<?php
+	include 'get_connection.php';
+
+	try {
+		if(PHP_SAPI === 'cli')
+		{
+			$state_id = $argv[1];
+			$county_id = $argv[2];
+			$city_id = $argv[3];
+		}
+			
+		else
+		{
+			if(isset($_GET['state']))
+				$state_id = $_GET['state'];
+			else $state_id = -1;
+			
+			if(isset($_GET['county']))
+				$county_id = $_GET['county'];	
+			else
+				$county_id = -1;
+			
+			if(isset($_GET['city']))
+				$city_id = $_GET['city'];
+			else $city_id = -1;
+		}
+			
+		get_recent_state_posts($conn, $state_id);
+		
+	}
+	catch (PDOException $e)
+	{
+		echo "Connection failed: " . $e->getMessage();
+		return;
+	}
+
+	$conn = null;
+
+	function get_recent_state_posts($conn, $state_id, $county_id, $city_id)
+	{
+		if($state_id != -1)
+		{
+			$query = $conn->prepare("SELECT posts.author,posts.title FROM posts WHERE posts.state = :state_id AND posts.county = -1 ORDER BY posts.ts DESC LIMIT 5;");
+			$query->bindparam(':state_id', $state_id);
+		}
+
+		if($county_id != -1)
+		{
+			$query = $conn->prepare("SELECT posts.author,posts.title FROM posts WHERE posts.county = :county_id AND posts.city = -1 ORDER BY posts.ts DESC LIMIT 5;");
+			$query->bindparam(':county_id', $county_id);
+		}
+
+		if($city_id != -1)
+		{
+			$query = $conn->prepare("SELECT posts.author,posts.title FROM posts WHERE posts.city = :city_id ORDER BY posts.ts DESC LIMIT 5;");
+			$query->bindparam(':city_id', $city_id);
+		}
+
+		$query->exeecute();
+		$results = $query->fetchAll();
+
+		echo $results;
+	}
+?>
