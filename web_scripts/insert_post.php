cdcfb3743a38454f4e28330233560d3256e89ee2
Christopher <beiercm@gmail.com>
2015-10-10 15:22:47 -0400

    Changes

--- web_scripts/insert_post.php
+++ web_scripts/insert_post.php
@@ -4,20 +4,20 @@
 
 	try {
 		$author = $_GET['author'];
-		$title = $_GET['title'];
-		$content = $_GET['content'];
+		$title = $_GET['#038;title'];
+		$content = $_GET['#038;content'];
 		
-		if(isset($_GET['state']))
-				$state_id = $_GET['state'];
+		if(isset($_GET['#038;state']))
+				$state_id = $_GET['#038;state'];
 		else $state_id = -1;
 			
-		if(isset($_GET['county']))
-			$county_id = $_GET['county'];	
+		if(isset($_GET['#038;county']))
+			$county_id = $_GET['#038;county'];	
 		else
 			$county_id = -1;
 		
-		if(isset($_GET['city']))
-			$city_id = $_GET['city'];
+		if(isset($_GET['#038;city']))
+			$city_id = $_GET['#038;city'];
 		else $city_id = -1;
 
 		insert_post($conn, $author, $title, $content, $state, $county, $city);
