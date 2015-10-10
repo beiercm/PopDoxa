07fef2cc395111c4581df5d1a040346352976713
Christopher <beiercm@gmail.com>
2015-10-10 14:41:44 -0400

    Changes

---
+++ web_scripts/get_county_name.php
@@ -0,0 +1,37 @@
+<?php
+	include 'get_connection.php';
+
+	try {
+		if(PHP_SAPI === 'cli')
+		{
+			$county = $argv[1];
+		}
+		else
+		{
+			$county = $_GET['county'];
+		}
+
+		get_county_name($conn, $county);
+	}
+	catch (PDOException $e)
+	{
+		echo "Connection failed: " . $e->getMessage();
+		return;
+	}
+
+	$conn = null;
+
+	function get_county_name($conn, $county)
+	{
+		$stmt = "SELECT name FROM counties where id = :county";
+
+		$sth = $conn->prepare($stmt);
+		$sth->bindparam(':county', $county);
+		$sth->execute();
+		$result = $sth->fetchAll();
+		
+		echo $result['name'];
+		
+	}
+
+?>
