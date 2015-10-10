2775d0f7819aad09e542f73fda1a0606a14f73a7
Christopher <beiercm@gmail.com>
2015-10-10 13:44:34 -0400

    Changes

--- web_scripts/get_posts.php
+++ web_scripts/get_posts.php
@@ -39,14 +39,14 @@
 
 		if($county_id != -1)
 		{
-			$query= $conn->prepare("SELECT users.username,posts.title,posts.views,posts.replies,posts.ts from posts join users on posts.author = users.id where county = :county_id AND city = -1;");
+			$query= $conn->prepare("SELECT users.username,posts.title,posts.views,posts.replies,posts.ts from posts join users on posts.author = users.id where posts.county = :county_id AND posts.city = -1;");
 			$query->bindparam(':county_id', $county_id);
 			$url = "10.171.204.135/forum_id.html?city =". $city_id;
 		}
 
 		if($city_id != -1)
 		{
-			$query = $conn->prepare("SELECT users.username,posts.title,posts.views,posts.replies,posts.ts from posts join users on posts.author = users.id where city =  :city_id;");
+			$query = $conn->prepare("SELECT users.username,posts.title,posts.views,posts.replies,posts.ts from posts join users on posts.author = users.id where posts.city =  :city_id;");
 			$query->bindparam(':city_id', $city_id);
 
 			$url = "10.171.204.135/forum_id.html?county =". $county_id;
@@ -54,7 +54,7 @@
 
 		if($county_id == -1 && $city_id == -1)
 		{
-			$query = $conn->prepare("SELECT users.username,posts.title,posts.views,posts.replies,posts.ts from posts join users on posts.author = users.id from posts where state = :state_id AND county = -1 AND city = -1;");
+			$query = $conn->prepare("SELECT users.username,posts.title,posts.views,posts.replies,posts.ts from posts join users on posts.author = users.id where posts.state = :state_id AND posts.county = -1 AND posts.city = -1;");
 			$query->bindparam(':state_id', $state_id);
 			$url = "10.171.204.135/forum_id.html?state =". $state_id;
 		}
