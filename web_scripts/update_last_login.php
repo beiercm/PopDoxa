c50ed3bc205aa75e395d96a14f20a4c3d7c8838e
Christopher <beiercm@gmail.com>
2015-10-17 12:40:12 -0400

    Changes

---
+++ web_scripts/update_last_login.php
@@ -0,0 +1,32 @@
+<?php
+
+	include 'get_connection.php';
+
+	try {
+		if(PHP_SAPI === 'cli')
+		{
+			$user_id = $argv[1];
+		}
+		else
+		{	
+			$user_id = $_GET['user_id'];
+		}	
+
+		update_last_login($conn, $user_id);
+	}
+	catch (PDOException $e)
+	{
+		echo "Connection failed: " . $e->getMessage();
+		return;
+	}
+
+	$conn = null;
+
+	function update_last_login($conn, $user_id)
+	{
+		$stmt = $conn->prepare("UPDATE users SET last_login = now() WHERE id = :user_id");
+		$stmt->bindparam(':user_id', $user_id);
+		$stmt->execute();
+	}
+
+?>
