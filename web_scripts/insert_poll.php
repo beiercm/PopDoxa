02b06257ab86eabf4b01942f1e31604c8eb3be51
Christopher <beiercm@gmail.com>
2015-10-20 23:20:24 -0400

    Changes

---
+++ web_scripts/insert_poll.php
@@ -0,0 +1,45 @@
+<?php
+
+	include 'get_connection.php';
+
+	try {
+		$author = $_GET['author'];
+		$question = $_GET['question'];
+
+		if(isset($_GET['state']))
+				$state = $_GET['state'];
+			else $state = -1;
+			
+			if(isset($_GET['county']))
+				$county = $_GET['county'];	
+			else
+				$county = -1;
+			
+			if(isset($_GET['city']))
+				$city = $_GET['city'];
+			else $city = -1;
+
+		insert_post($conn, $author, $question, $state, $county, $city);
+	}
+	catch (PDOException $e)
+	{
+		echo "Connection failed: " . $e->getMessage();
+		return;
+	}
+
+	$conn = null;
+
+	function insert_post($conn, $author, $question, $content, $state, $county, $city)
+	{
+		echo $state;
+
+		$stmt = $conn->prepare("INSERT INTO polls (author, question, state, county, city) VALUES (:author,:question,:state,:county,:city);");
+		$stmt->bindparam(':author', $author);
+		$stmt->bindparam(':question', $question);
+		$stmt->bindparam(':state', $state);
+		$stmt->bindparam(':county', $county);
+		$stmt->bindparam(':city', $city);
+
+		$stmt->execute();
+	}
+?>
