ae7a63e679ef84c90ca4a0a88ac81bc86ab99008
Christopher <beiercm@gmail.com>
2015-10-26 21:25:36 -0400

    Changes

--- web_scripts/get_poll_results.php
+++ web_scripts/get_poll_results.php
@@ -9,6 +9,13 @@
 		else
 		{
 			$poll_id = $_GET['poll_id'];
+
+			if(isset($_GET['opin_id']))
+			{
+				$opin_id = $_GET['opin_id'];
+			}
+			else
+				$opin_id = -1;
 		}
 
 		get_poll_results($conn, $poll_id);
@@ -21,31 +28,92 @@
 
 	$conn = null;
 
-	function get_poll_results($conn, $poll_id)
+	function get_poll_results($conn, $poll_id, $opin_id)
 	{
-		$stmt = "select count(vote) from poll_results where vote = 'y' AND poll_id = :poll_id";
-		$stmt = $conn->prepare($stmt);
-		$stmt->bindparam(':poll_id', $poll_id);
-		$stmt->execute();
-		$yes_results = $stmt->fetchall();
+		if(strcmp($opin_id, -1))
+		{
+			$stmt = "select count(vote) from poll_results where vote = 'y' AND poll_id = :poll_id";
+			$stmt = $conn->prepare($stmt);
+			$stmt->bindparam(':poll_id', $poll_id);
+			$stmt->execute();
+			$yes_results = $stmt->fetchall();
 
-		$stmt = "select count(vote) from poll_results where vote = 'n' AND poll_id = :poll_id";
-		$stmt = $conn->prepare($stmt);
-		$stmt->bindparam(':poll_id', $poll_id);
-		$stmt->execute();
-		$no_results = $stmt->fetchall();
+			$stmt = "select count(vote) from poll_results where vote = 'n' AND poll_id = :poll_id";
+			$stmt = $conn->prepare($stmt);
+			$stmt->bindparam(':poll_id', $poll_id);
+			$stmt->execute();
+			$no_results = $stmt->fetchall();
 
-		$stmt = "select count(vote) from poll_results where vote = 'u' AND poll_id = :poll_id";
-		$stmt = $conn->prepare($stmt);
-		$stmt->bindparam(':poll_id', $poll_id);
-		$stmt->execute();
-		$undecided_results = $stmt->fetchall();
+			$stmt = "select count(vote) from poll_results where vote = 'u' AND poll_id = :poll_id";
+			$stmt = $conn->prepare($stmt);
+			$stmt->bindparam(':poll_id', $poll_id);
+			$stmt->execute();
+			$undecided_results = $stmt->fetchall();
 
-		$results['yes'] = $yes_results[0][0];
-		$results['no'] = $no_results[0][0];
-		$results['undecided'] = $undecided_results[0][0];
+			$results['yes'] = $yes_results[0][0];
+			$results['no'] = $no_results[0][0];
+			$results['undecided'] = $undecided_results[0][0];
 
-		echo json_encode($results);
+			echo json_encode($results);
+		}
+		else
+		{
+			$stmt = 
+			"
+			SELECT count(uo.user_id)
+			from user_opin as uo
+			join poll_results as pr
+			on uo.user_id = pr.user_id
+			join opinions as op
+			on uo.opin_id = op.id
+			where uo.opin_id = :opin_id
+			and pr.poll_id = :poll_id
+			and pr.vote = 'y';";
+			$stmt = $conn->prepare($stmt);
+			$stmt->bindparam(':poll_id', $poll_id);
+			$stmt->bindparam(':opin_id', $opin_id);
+			$stmt->execute();
+			$yes_results = $stmt->fetchall();
 
+			$stmt = 
+			"
+			SELECT count(uo.user_id)
+			from user_opin as uo
+			join poll_results as pr
+			on uo.user_id = pr.user_id
+			join opinions as op
+			on uo.opin_id = op.id
+			where uo.opin_id = :opin_id
+			and pr.poll_id = :poll_id
+			and pr.vote = 'n';";
+			$stmt = $conn->prepare($stmt);
+			$stmt->bindparam(':poll_id', $poll_id);
+			$stmt->bindparam(':opin_id', $opin_id);
+			$stmt->execute();
+			$no_results = $stmt->fetchall();
+
+			$stmt = 
+			"
+			SELECT count(uo.user_id)
+			from user_opin as uo
+			join poll_results as pr
+			on uo.user_id = pr.user_id
+			join opinions as op
+			on uo.opin_id = op.id
+			where uo.opin_id = :opin_id
+			and pr.poll_id = :poll_id
+			and pr.vote = 'n';";
+			$stmt = $conn->prepare($stmt);
+			$stmt->bindparam(':poll_id', $poll_id);
+			$stmt->bindparam(':opin_id', $opin_id);
+			$stmt->execute();
+			$undecided_results = $stmt->fetchall();
+
+			$results['yes'] = $yes_results[0][0];
+			$results['no'] = $no_results[0][0];
+			$results['undecided'] = $undecided_results[0][0];
+
+			echo json_encode($results);
+		}
 	}
 ?>
