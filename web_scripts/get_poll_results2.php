4332cac49e30d9205d5257a34a992c457d618efa
Christopher <beiercm@gmail.com>
2015-11-04 17:05:39 -0500

    Changes

--- web_scripts/get_poll_results2.php
+++ web_scripts/get_poll_results2.php
@@ -36,139 +36,265 @@
 
 		for($i = 1; $i <= count($opinions); $i++)
 		{
-			$query = 
-			"
-			SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			$query = "
+			SELECT op.opin_name, p.question, count(uo.user_id), uo.opinion, pr.vote
 			from user_opin as uo
 			join poll_results as pr
 			on uo.user_id = pr.user_id
 			join opinions as op
 			on uo.opin_id = op.id
+			join polls as p
+			on p.id = pr.poll_id
 			where uo.opin_id = " . $i . "
 			and uo.opinion = 'f'
 			and pr.poll_id = :poll_id
 			and pr.vote = 'y'
 			UNION
-			SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			SELECT op.opin_name, p.question, count(uo.user_id), uo.opinion, pr.vote
 			from user_opin as uo
 			join poll_results as pr
 			on uo.user_id = pr.user_id
 			join opinions as op
 			on uo.opin_id = op.id
+			join polls as p
+			on p.id = pr.poll_id
 			where uo.opin_id = " . $i . "
 			and uo.opinion = 'a'
 			and pr.poll_id = :poll_id
 			and pr.vote = 'y'
 			UNION
-			SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			SELECT op.opin_name, p.question, count(uo.user_id), uo.opinion, pr.vote
 			from user_opin as uo
 			join poll_results as pr
 			on uo.user_id = pr.user_id
 			join opinions as op
 			on uo.opin_id = op.id
+			join polls as p
+			on p.id = pr.poll_id
 			where uo.opin_id = " . $i . "
 			and uo.opinion = 'n'
 			and pr.poll_id = :poll_id
-			and pr.vote = 'y';
-			";	
-
-			$query = $conn->prepare($query);
-			$query->bindparam(':poll_id', $poll_id);
-			$query->execute();
-
-			$yes_results = $query->fetchall();
-
-			$query = 
-			"
-			SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			and pr.vote = 'y'
+			UNION
+			SELECT op.opin_name, p.question, count(uo.user_id), uo.opinion, pr.vote
 			from user_opin as uo
 			join poll_results as pr
 			on uo.user_id = pr.user_id
 			join opinions as op
 			on uo.opin_id = op.id
+			join polls as p
+			on p.id = pr.poll_id
 			where uo.opin_id = " . $i . "
 			and uo.opinion = 'f'
 			and pr.poll_id = :poll_id
 			and pr.vote = 'n'
 			UNION
-			SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			SELECT op.opin_name, p.question, count(uo.user_id), uo.opinion, pr.vote
 			from user_opin as uo
 			join poll_results as pr
 			on uo.user_id = pr.user_id
 			join opinions as op
 			on uo.opin_id = op.id
+			join polls as p
+			on p.id = pr.poll_id
 			where uo.opin_id = " . $i . "
 			and uo.opinion = 'a'
 			and pr.poll_id = :poll_id
 			and pr.vote = 'n'
 			UNION
-			SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			SELECT op.opin_name, p.question, count(uo.user_id), uo.opinion, pr.vote
 			from user_opin as uo
 			join poll_results as pr
 			on uo.user_id = pr.user_id
 			join opinions as op
 			on uo.opin_id = op.id
+			join polls as p
+			on p.id = pr.poll_id
 			where uo.opin_id = " . $i . "
 			and uo.opinion = 'n'
 			and pr.poll_id = :poll_id
-			and pr.vote = 'n';
-			";	
-
-			$query = $conn->prepare($query);
-			$query->bindparam(':poll_id', $poll_id);
-			$query->execute();
-
-			$no_results = $query->fetchall();
-
-			$query = 
-			"
-			SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			and pr.vote = 'n'
+			UNION
+			SELECT op.opin_name, p.question, count(uo.user_id), uo.opinion, pr.vote
 			from user_opin as uo
 			join poll_results as pr
 			on uo.user_id = pr.user_id
 			join opinions as op
 			on uo.opin_id = op.id
+			join polls as p
+			on p.id = pr.poll_id
 			where uo.opin_id = " . $i . "
 			and uo.opinion = 'f'
 			and pr.poll_id = :poll_id
 			and pr.vote = 'u'
 			UNION
-			SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			SELECT op.opin_name, p.question, count(uo.user_id), uo.opinion, pr.vote
 			from user_opin as uo
 			join poll_results as pr
 			on uo.user_id = pr.user_id
 			join opinions as op
 			on uo.opin_id = op.id
+			join polls as p
+			on p.id = pr.poll_id
 			where uo.opin_id = " . $i . "
 			and uo.opinion = 'a'
 			and pr.poll_id = :poll_id
 			and pr.vote = 'u'
 			UNION
-			SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			SELECT op.opin_name, p.question, count(uo.user_id), uo.opinion, pr.vote
 			from user_opin as uo
 			join poll_results as pr
 			on uo.user_id = pr.user_id
 			join opinions as op
 			on uo.opin_id = op.id
+			join polls as p
+			on p.id = pr.poll_id
 			where uo.opin_id = " . $i . "
 			and uo.opinion = 'n'
 			and pr.poll_id = :poll_id
 			and pr.vote = 'u';
-			";	
+			";
 
 			$query = $conn->prepare($query);
 			$query->bindparam(':poll_id', $poll_id);
 			$query->execute();
-
-			$undecided_results = $query->fetchall();
-
-			$results['yes'] = $yes_results;
-			$results['no'] = $no_results;
-			$results['undecided'] = $undecided_results;
+			$results = $query->fetchall();
 
 			echo json_encode($results);
 
-			break;
+			// $query = 
+			// "
+			// SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			// from user_opin as uo
+			// join poll_results as pr
+			// on uo.user_id = pr.user_id
+			// join opinions as op
+			// on uo.opin_id = op.id
+			// where uo.opin_id = " . $i . "
+			// and uo.opinion = 'f'
+			// and pr.poll_id = :poll_id
+			// and pr.vote = 'y'
+			// UNION
+			// SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			// from user_opin as uo
+			// join poll_results as pr
+			// on uo.user_id = pr.user_id
+			// join opinions as op
+			// on uo.opin_id = op.id
+			// where uo.opin_id = " . $i . "
+			// and uo.opinion = 'a'
+			// and pr.poll_id = :poll_id
+			// and pr.vote = 'y'
+			// UNION
+			// SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			// from user_opin as uo
+			// join poll_results as pr
+			// on uo.user_id = pr.user_id
+			// join opinions as op
+			// on uo.opin_id = op.id
+			// where uo.opin_id = " . $i . "
+			// and uo.opinion = 'n'
+			// and pr.poll_id = :poll_id
+			// and pr.vote = 'y';
+			// ";	
+
+			// $query = $conn->prepare($query);
+			// $query->bindparam(':poll_id', $poll_id);
+			// $query->execute();
+
+			// $yes_results = $query->fetchall();
+
+			// $query = 
+			// "
+			// SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			// from user_opin as uo
+			// join poll_results as pr
+			// on uo.user_id = pr.user_id
+			// join opinions as op
+			// on uo.opin_id = op.id
+			// where uo.opin_id = " . $i . "
+			// and uo.opinion = 'f'
+			// and pr.poll_id = :poll_id
+			// and pr.vote = 'n'
+			// UNION
+			// SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			// from user_opin as uo
+			// join poll_results as pr
+			// on uo.user_id = pr.user_id
+			// join opinions as op
+			// on uo.opin_id = op.id
+			// where uo.opin_id = " . $i . "
+			// and uo.opinion = 'a'
+			// and pr.poll_id = :poll_id
+			// and pr.vote = 'n'
+			// UNION
+			// SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			// from user_opin as uo
+			// join poll_results as pr
+			// on uo.user_id = pr.user_id
+			// join opinions as op
+			// on uo.opin_id = op.id
+			// where uo.opin_id = " . $i . "
+			// and uo.opinion = 'n'
+			// and pr.poll_id = :poll_id
+			// and pr.vote = 'n';
+			// ";	
+
+			// $query = $conn->prepare($query);
+			// $query->bindparam(':poll_id', $poll_id);
+			// $query->execute();
+
+			// $no_results = $query->fetchall();
+
+			// $query = 
+			// "
+			// SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			// from user_opin as uo
+			// join poll_results as pr
+			// on uo.user_id = pr.user_id
+			// join opinions as op
+			// on uo.opin_id = op.id
+			// where uo.opin_id = " . $i . "
+			// and uo.opinion = 'f'
+			// and pr.poll_id = :poll_id
+			// and pr.vote = 'u'
+			// UNION
+			// SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			// from user_opin as uo
+			// join poll_results as pr
+			// on uo.user_id = pr.user_id
+			// join opinions as op
+			// on uo.opin_id = op.id
+			// where uo.opin_id = " . $i . "
+			// and uo.opinion = 'a'
+			// and pr.poll_id = :poll_id
+			// and pr.vote = 'u'
+			// UNION
+			// SELECT op.opin_name, count(uo.user_id), uo.opinion, pr.vote
+			// from user_opin as uo
+			// join poll_results as pr
+			// on uo.user_id = pr.user_id
+			// join opinions as op
+			// on uo.opin_id = op.id
+			// where uo.opin_id = " . $i . "
+			// and uo.opinion = 'n'
+			// and pr.poll_id = :poll_id
+			// and pr.vote = 'u';
+			// ";	
+
+			// $query = $conn->prepare($query);
+			// $query->bindparam(':poll_id', $poll_id);
+			// $query->execute();
+
+			// $undecided_results = $query->fetchall();
+
+			// $results['yes'] = $yes_results;
+			// $results['no'] = $no_results;
+			// $results['undecided'] = $undecided_results;
+
+			// echo json_encode($results);
+
+			// break;
 		}
 
 		// $query = 
