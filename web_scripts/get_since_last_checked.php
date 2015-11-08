605f8925c219e95a9b6c5760d2e4d02853a41baa
DarthEnchilada <beiercm@gmail.com>
2015-11-08 14:23:51 -0500

    c

---
+++ web_scripts/get_since_last_checked.php
@@ -0,0 +1,53 @@
+<?php
+	include 'get_connection.php';
+
+	try {
+		if(PHP_SAPI === 'cli')
+		{
+			$user_id = $argv[1];
+		}
+		else
+		{
+			$user_id = $_GET['user_id'];
+		}
+
+		get_since_last_checked($conn, $user_id);
+	}
+	catch (PDOException $e)
+	{
+		echo "Connection failed: " . $e->getMessage();
+		return;
+	}
+
+	$conn = null;
+
+	function get_since_last_checked($conn, $user_id)
+	{
+		$query = "
+			SELECT polls.question, polls.id
+			from polls
+			join users
+			on polls.ts > users.last_checked
+			where polls.state = users.state
+			or polls.county = users.county
+			or polls.city = users.city
+			and users.id = 1
+			UNION
+			SELECT posts.title, posts.id
+			from posts
+			join users
+			on posts.ts > users.last_checked
+			where posts.state = users.state
+			or posts.county = users.county
+			or posts.city = users.city
+			and users.id = 1;
+			";
+
+		$query = $conn->prepare($query);
+		$query->bindparam(':user_id', $user_id);
+		$query->execute();
+		$results = $query->fetchall();
+
+		echo json_encode($results);
+	}
+?>
