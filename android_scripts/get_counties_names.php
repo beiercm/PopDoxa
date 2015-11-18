0847f9b0603ccdda9dad0cd493d4aa756d51ab87
Christopher <beiercm@gmail.com>
2015-11-18 15:37:55 -0500

    Changes

--- android_scripts/get_counties_names.php
+++ android_scripts/get_counties_names.php
@@ -2,24 +2,25 @@
 	include 'get_connection.php';
 
 	if(PHP_SAPI === 'cli')
-		{
-			$state_id = $argv[1];
-		}
-		else
-		{
-			$state_id = $_GET['state_id'];
-		}
+	{
+		$state_id = $argv[1];
+	}
+	else
+	{
+		$state_id = $_GET['state_id'];
+	}
 
 	get_counties_name($conn, $state_id);
 	$conn = null;
 	
 	function get_counties_name($conn, $state_id)
 	{
-		$stmt = "SELECT name,id FROM counties";
+		$stmt = "SELECT name,id FROM counties where state_id = :state_id";
 
-		$sth = $conn->prepare($stmt);
-		$sth->execute();
-		$result = $sth->fetchAll();
+		$stmt = $conn->prepare($stmt);
+		$stmt->bindparam(':state_id', $state_id);
+		$stmt->execute();
+		$result = $stmt->fetchAll();
 
 		echo json_encode($result);
 	}
