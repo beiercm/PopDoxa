bf1c4b8de0924d512b0e8e955dc56ba2c3eac16b
Christopher <beiercm@gmail.com>
2015-11-21 11:15:26 -0500

    Changes

--- web_scripts/insert_vote.php
+++ web_scripts/insert_vote.php
@@ -19,7 +19,27 @@
 
 	function insert_vote($conn, $user_id, $poll_id, $vote)
 	{
-		$stmt = $conn->prepare("INSERT INTO poll_results (user_id, poll_id, vote) VALUES (:user_id,:poll_id,:vote);");
+		$stmt = "
+			SELECT user_id, poll_id, vote
+			FROM poll_results
+			where user_id = :user_id
+			AND poll_id = :poll_id;"
+
+		$stmt = $conn->prepare($stmt);
+		$stmt->bindparam(':user_id', $user_id);
+		$stmt->bindparam(':poll_id', $poll_id);
+		$stmt->execute();
+		$results = $stmt->fetchall();
+
+		if(count($results) > 0)
+			return;
+		
+		$stmt = "
+			INSERT INTO poll_results 
+			(user_id, poll_id, vote) 
+			VALUES (:user_id,:poll_id,:vote);"
+
+		$stmt = $conn->prepare($stmt);
 		$stmt->bindparam(':user_id', $user_id);
 		$stmt->bindparam(':poll_id', $poll_id);
 		$stmt->bindparam(':vote', $vote);
