743464cdfecf1c86c5c6d6d1057ab20841500cbe
kwaiidan <philip.rajala@gmail.com>
2015-11-30 03:56:46 -0500

    Update get_counties_names.php

--- android_scripts/get_counties_names.php
+++ android_scripts/get_counties_names.php
@@ -3,22 +3,22 @@
 
 	if(PHP_SAPI === 'cli')
 	{
-		$state_id = $argv[1];
+		$state_name = $argv[1];
 	}
 	else
 	{
-		$state_id = $_GET['state_id'];
+		$state_name = $_GET['state_name'];
 	}
 
-	get_counties_name($conn, $state_id);
+	get_counties_name($conn, $state_name);
 	$conn = null;
 	
-	function get_counties_name($conn, $state_id)
+	function get_counties_name($conn, $state_name)
 	{
-		$stmt = "SELECT name,id FROM counties where state_id = :state_id";
+		$stmt = "SELECT name FROM counties where state_name = :state_name";
 
 		$stmt = $conn->prepare($stmt);
-		$stmt->bindparam(':state_id', $state_id);
+		$stmt->bindparam(':state_name', $state_name);
 		$stmt->execute();
 		$result = $stmt->fetchAll();
 
