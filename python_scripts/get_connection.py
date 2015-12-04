0c38cbbe84809b654e7332904a41de1fa593b5d0
DarthEnchilada <beiercm@gmail.com>
2015-12-03 19:22:01 -0500

    c

--- python_scripts/get_connection.py
+++ python_scripts/get_connection.py
@@ -4,10 +4,7 @@ from mysql.connector import errorcode
 def connection():
 	try:
 		login_info = list(open("/home/christopher/login.txt"))
-		conn = mysql.connector.connect(user=login_info[1].strip(), password=login_info[2].strip(), database='cbpopdoxa')
-		query = "create database if not exists hobby_tracker"
-		conn.cursor().execute(query)
-
+		conn = mysql.connector.connect(user=login_info[1].strip(), password=login_info[2].strip())
 		return conn
 
 	except errorcode:
