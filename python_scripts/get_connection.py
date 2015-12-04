5770b4234b9be612e522123f61e91f0718f7c9ca
DarthEnchilada <beiercm@gmail.com>
2015-12-03 20:48:34 -0500

    c

--- python_scripts/get_connection.py
+++ python_scripts/get_connection.py
@@ -5,6 +5,11 @@ def connection():
 	try:
 		login_info = list(open("/home/christopher/login.txt"))
 		conn = mysql.connector.connect(user=login_info[1].strip(), password=login_info[2].strip())
+		try:
+			query = "use cbpopdoxa;"
+			conn.cursor().execute(query)
+		except Exception as e:
+			print e
 		return conn
 
 	except errorcode:
