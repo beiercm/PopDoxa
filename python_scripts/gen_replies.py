79ba9fd698aec0304950e151bf5af9e5702391d5
Christopher Beier <beiercm@gmail.com>
2015-12-05 13:09:23 -0500

    c

--- python_scripts/gen_replies.py
+++ python_scripts/gen_replies.py
@@ -7,45 +7,26 @@ cursor = conn.cursor()
 
 def start(n):
 	#email = emails[random.randint(0, len(emails) - 1)]
+	with open("replies.txt", 'w+') as f_out:
+		reply_count = 0
 
-	reply_count = 0
+		query = "SELECT id, state, county, city from users"
+		cursor.execute(query)
+		users = cursor.fetchall()
 
-	query = "SELECT id, state, county, city from users"
-	cursor.execute(query)
-	users = cursor.fetchall()
+		query = "SELECT id, state, county, city from posts"
+		cursor.execute(query)
+		posts = cursor.fetchall()
 
-	print users
+		while reply_count < n:
+			post = posts[random.randint(0, len(posts) - 1)]
 
-	query = "SELECT id, state, county, city from posts"
-	cursor.execute(query)
-	posts = cursor.fetchall()
+			for user in users:
+				if user[1] == post[1] or user[2] == post[2] or user[3] == post[3]:
 
-	print posts
-
-	while reply_count < n:
-		post = posts[random.randint(0, len(posts) - 1)]
-
-		for user in users:
-			#if user[1] == post[1] or user[2] == post[2] or user[3] == post[3]:
-			if user[2] == post[2]:
-				print user, post
-				break
-
-	# 	query = "SELECT state, county, city from posts where id = " + str(post_id)
-	# 	cursor.execute(query)
-
-	# 	results = cursor.fetchall()
-
-	# 	print results
-
-		# user_id = random.randint(0, 1000)
-
-
-
-		# content = mk.generate_sentence(random.randint(2, 5))
-
-		# print(user_id)
-		# print(post_id)
-		# print(content)
+					content = mk.generate_sentence(random.randint(2, 5))
+					f_out.write(user_id)
+					f_out.write(post_id)
+					f_out.write(content)
 
 start(100)
