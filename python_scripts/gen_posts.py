b56ede512f00f4a32cda2534133fda2803802097
Christopher Beier <beiercm@gmail.com>
2015-12-05 14:03:05 -0500

    c

--- python_scripts/gen_posts.py
+++ python_scripts/gen_posts.py
@@ -1,45 +1,45 @@
 import os, random
-import get_connection as gc
+from get_connection import data_path, connection
 import markov as mk
 
-conn = gc.connection()
+conn = connection()
 cursor = conn.cursor()
 
 def start(n):
 	#email = emails[random.randint(0, len(emails) - 1)]
 
 	mg = mk.MarkovGenerator()
+	with open(data_path + "posts.txt", 'w+') as f_out:
+		query = "SELECT id, state, county, city from users"
+		cursor.execute(query)
+		users = cursor.fetchall()
 
-	query = "SELECT id, state, county, city from users"
-	cursor.execute(query)
-	users = cursor.fetchall()
+		for i in range(n):
+			state = -1
+			county = -1
+			city = -1
 
-	for i in range(n):
-		state = -1
-		county = -1
-		city = -1
+			user = users[random.randint(0, len(users) - 1)]
 
-		user = users[random.randint(0, len(users) - 1)]
+			title = mg.generate_sentence(1)
 
-		title = mg.generate_sentence(1)
+			content = mg.generate_sentence(random.randint(2, 5))
 
-		content = mg.generate_sentence(random.randint(2, 5))
+			location = random.randint(1, 4)
 
-		location = random.randint(1, 4)
-
-		if location == 1:
-			state = user[1]
-		if location == 2:
-			county = user[2]
-		if location == 3:
-			city = user[3]
+			if location == 1:
+				state = user[1]
+			if location == 2:
+				county = user[2]
+			if location == 3:
+				city = user[3]
 
 
-		print(user_id)
-		print(title)
-		print(content)
-		print(state)
-		print(county)
-		print(city)
+			f_out.write(user[0] + "\n")
+			f_out.write(title + "\n")
+			f_out.write(content + "\n")
+			f_out.write(state + "\n")
+			f_out.write(county + "\n")
+			f_out.write(city + "\n")
 
 start(1000)
