ea9618fb6573a2dc27df48cf00b14cde1aab299a
Christopher Beier <beiercm@gmail.com>
2015-12-05 14:16:06 -0500

    c

--- python_scripts/generate_test_data.py
+++ python_scripts/generate_test_data.py
@@ -1,9 +1,9 @@
 import gen_users as gu
-# import gen_posts as gp
-# import gen_replies as gr
-# import gen_user_opins as guo
-# import gen_poll_results as gpr
-# import gen_poll_replies as gpr2
+import gen_posts as gp
+import gen_replies as gr
+import gen_user_opins as guo
+import gen_poll_results as gpr
+import gen_poll_replies as gpr2
 
 def main():
 	users = 10000 
@@ -11,13 +11,13 @@ def main():
 	gu.start(users)
 
 	print "Creating posts..."
-	#gp.start(users * 2)
+	gp.start(users * 2)
 
 	print "Creating replies..."	
-	#gr.start(users * 3)
+	gr.start(users * 3)
 
 	print "Creating user opinions..."
-	#guo.start(users)
+	guo.start(users)
 
 	print "Creating poll replies..."
 	gpr2.start(users * 1.5)
