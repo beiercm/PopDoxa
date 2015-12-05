079e33ca64333d6f6cb94a4e3091937a2cb957e5
Christopher Beier <beiercm@gmail.com>
2015-12-05 18:20:34 -0500

    c

---
+++ web_scripts/get_feedback.php
@@ -0,0 +1,22 @@
+<?php
+
+	include 'get_connection.php';
+		
+	get_feedback($conn);
+	
+	$conn = null;
+
+	function get_feedback($conn)
+	{
+		$query = "SELECT user_id, content, ts FROM feedback";
+		$query = $conn->prepare($query);
+		$query->execute();
+
+		$results = $query->fetchall()
+
+		for($i = 0; $i < count($results); $i++)
+			echo $results[$i]['user_id'] . "\n" . $results[$i]['content'] . "\n" . $results[$i]['ts'] . "\n";
+
+	}
+
+?>
