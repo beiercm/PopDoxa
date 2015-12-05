05baa19e8c641afbdaa7bfb083e465be3fe5994c
Christopher Beier <beiercm@gmail.com>
2015-12-05 18:15:30 -0500

    c

---
+++ python_scripts/get_feedback.php
@@ -0,0 +1,28 @@
+<?php
+
+	include 'get_connection.php';
+		
+		get_feedback($conn);
+	
+	catch (PDOException $e)
+	{
+		echo "Connection failed: " . $e->getMessage();
+		return;
+	}
+
+	$conn = null;
+
+	function get_feedback($conn)
+	{
+		$query = "SELECT user_id, content, ts FROM feedback";
+		$query = $conn->prepare($query);
+		$query->execute();
+
+		$results = $query->fetchall()
+
+		for($i = 0; $i < count($results); $i++)
+			echo $results[$i]['user_id'] . "\n" . $results[$i]['content'] . "\n" . $results[$i]['ts'] . "\n";
+
+	}
+
+?>
