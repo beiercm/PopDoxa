6e2ebbb4a5a11b931c165684f1e0542a4c6c2595
Christopher Beier <beiercm@gmail.com>
2015-12-05 10:04:07 -0500

    c

--- web_scripts/check_use_pw.php
+++ web_scripts/check_use_pw.php
@@ -27,6 +27,6 @@
 	function get_user_id_w_username($conn, $username)
 	{
 		echo password_hash($username, PASSWORD_DEFAULT);
-		echo password_verify($username, password_hash($username));
+		echo password_verify($username, password_hash($username, PASSWORD_DEFAULT));
 	}
 ?>
